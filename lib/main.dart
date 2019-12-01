839e373d28df0a4d8ab4c7aa1538184b817160ad
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2019-12-01 17:54:32 -0300

    Correções do código da Ellen

---
+++ lib/main.dart
@@ -0,0 +1,261 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:beacon/widgets/beacon_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:flutter_blue_beacon/flutter_blue_beacon.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+void main() {
+  runApp(new FlutterBlueApp());
+}
+
+class FlutterBlueApp extends StatefulWidget {
+  FlutterBlueApp({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _FlutterBlueAppState createState() => new _FlutterBlueAppState();
+}
+
+enum TtsState { playing, stopped }
+
+class _FlutterBlueAppState extends State<FlutterBlueApp> {
+  FlutterBlueBeacon flutterBlueBeacon = FlutterBlueBeacon.instance;
+  FlutterBlue _flutterBlue = FlutterBlue.instance;
+
+  // Para a sala
+  String _roomHashCode = 'dsagdkjahdjkhdsaj2e12d123d13e1';
+  String _roomName = 'Sala de apresentação dos trabalhos de conclusão de curso';
+  double _roomDistance = 1.0;
+
+  // Para a parte de speech
+  FlutterTts flutterTts;
+  dynamic languages;
+  dynamic voices;
+  String language;
+  String voice;
+  int silencems;
+  String _newVoiceText;
+  TtsState ttsState = TtsState.stopped;
+  get isPlaying => ttsState == TtsState.playing;
+  get isStopped => ttsState == TtsState.stopped;
+
+  /// Scanning
+  StreamSubscription _scanSubscription;
+  Map<int, Beacon> beacons = new Map();
+  bool isScanning = false;
+
+  /// State
+  StreamSubscription _stateSubscription;
+  BluetoothState state = BluetoothState.unknown;
+
+  @override
+  void initState() {
+    super.initState();
+    // Immediately get the state of FlutterBlue
+    _flutterBlue.state.then((s) {
+      setState(() {
+        state = s;
+      });
+    });
+    // Subscribe to state changes
+    _stateSubscription = _flutterBlue.onStateChanged().listen((s) {
+      setState(() {
+        state = s;
+      });
+    });
+
+    initTts();
+  }
+
+  initTts() {
+    flutterTts = FlutterTts();
+
+    if (Platform.isAndroid) {
+      flutterTts.ttsInitHandler(() {
+        _getLanguages();
+        _getVoices();
+      });
+    } else if (Platform.isIOS) {
+      _getLanguages();
+      _getVoices();
+    }
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        ttsState = TtsState.playing;
+      });
+    });
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        print("Complete");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future _getLanguages() async {
+    languages = await flutterTts.getLanguages;
+    if (languages != null) setState(() => languages);
+  }
+
+  Future _getVoices() async {
+    voices = await flutterTts.getVoices;
+    if (voices != null) setState(() => voices);
+  }
+
+  Future _speak() async {
+    if (_newVoiceText != null) {
+      if (_newVoiceText.isNotEmpty) {
+        var result = await flutterTts.speak(_newVoiceText);
+        if (result == 1) setState(() => ttsState = TtsState.playing);
+      }
+    }
+  }
+
+  Future _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _stateSubscription?.cancel();
+    _stateSubscription = null;
+    _scanSubscription?.cancel();
+    _scanSubscription = null;
+    flutterTts.stop();
+  }
+
+  _clearAllBeacons() {
+    setState(() {
+      beacons = Map<int, Beacon>();
+    });
+  }
+
+  _startScan() {
+    print("Scanning now");
+    _scanSubscription = flutterBlueBeacon
+        .scan(timeout: const Duration(seconds: 20))
+        .listen((beacon) {
+      print('localName: ${beacon.scanResult.advertisementData.localName}');
+      print(
+          'manufacturerData: ${beacon.scanResult.advertisementData.manufacturerData}');
+      print('serviceData: ${beacon.scanResult.advertisementData.serviceData}');
+      setState(() {
+        beacons[beacon.hash] = beacon;
+      });
+    }, onDone: _stopScan);
+
+    setState(() {
+      isScanning = true;
+    });
+  }
+
+  _stopScan() {
+    print("Scan stopped");
+    _scanSubscription?.cancel();
+    _scanSubscription = null;
+    setState(() {
+      isScanning = false;
+    });
+  }
+
+  _buildScanningButton() {
+    if (state != BluetoothState.on) {
+      return null;
+    }
+    if (isScanning) {
+      return new FloatingActionButton(
+        child: new Icon(Icons.stop),
+        onPressed: _stopScan,
+        backgroundColor: Colors.red,
+      );
+    } else {
+      return new FloatingActionButton(
+          child: new Icon(Icons.search), onPressed: _startScan);
+    }
+  }
+
+  _buildScanResultTiles() {
+    return beacons.values.map<Widget>((b) {
+
+      if (b.distance <= _roomDistance){
+        _newVoiceText = _roomName;
+        _speak();
+      }
+
+      if (b is IBeacon) {
+        return IBeaconCard(iBeacon: b);
+      }
+      if (b is EddystoneUID) {
+        return EddystoneUIDCard(eddystoneUID: b);
+      }
+      if (b is EddystoneEID) {
+        return EddystoneEIDCard(eddystoneEID: b);
+      }
+      return Card();
+    }).toList();
+  }
+
+  _buildAlertTile() {
+    return new Container(
+      color: Colors.redAccent,
+      child: new ListTile(
+        title: new Text(
+          'Bluetooth adapter is ${state.toString().substring(15)}',
+          style: Theme.of(context).primaryTextTheme.subhead,
+        ),
+        trailing: new Icon(
+          Icons.error,
+          color: Theme.of(context).primaryTextTheme.subhead.color,
+        ),
+      ),
+    );
+  }
+
+  _buildProgressBarTile() {
+    return new LinearProgressIndicator();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var tiles = new List<Widget>();
+    if (state != BluetoothState.on) {
+      tiles.add(_buildAlertTile());
+    }
+
+    tiles.addAll(_buildScanResultTiles());
+
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('FlutterBlueBeacon Example'),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.clear), onPressed: _clearAllBeacons)
+          ],
+        ),
+        floatingActionButton: _buildScanningButton(),
+        body: new Stack(
+          children: <Widget>[
+            (isScanning) ? _buildProgressBarTile() : new Container(),
+            new ListView(
+              children: tiles,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
